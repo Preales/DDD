0a472aeac167b8e4dbb73f12d379c2b19dfb11bb
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2023-10-07 13:23:53 -0500

    🥅 Problems Details

--- src/EasyPos/Application/Customers/Create/CreateCustomerCommandHandler.cs
+++ src/EasyPos/Application/Customers/Create/CreateCustomerCommandHandler.cs
@@ -1,11 +1,12 @@
 using Domain.Customers;
 using Domain.Primitives;
 using Domain.ValueObjects;
+using ErrorOr;
 using MediatR;
 
 namespace Application.Customers.Create;
 
-internal sealed class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, Unit>
+internal sealed class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, ErrorOr<Unit>>
 {
     private readonly ICustomerRepository _customerRepository;
     private readonly IUnitOfWork _unitOfWork;
@@ -16,34 +17,41 @@ internal sealed class CreateCustomerCommandHandler : IRequestHandler<CreateCusto
         _unitOfWork = unitOfWork ?? throw new ArgumentException(nameof(_unitOfWork));
     }
 
-    public async Task<Unit> Handle(CreateCustomerCommand command, CancellationToken cancellationToken)
+    public async Task<ErrorOr<Unit>> Handle(CreateCustomerCommand command, CancellationToken cancellationToken)
     {
-        if(PhoneNumber.Create(command.PhoneNumber) is not PhoneNumber phoneNumber)
-            throw new ArgumentException(nameof(phoneNumber));
-
-        if(Address.Create(
-                command.Country,
-                command.Line1,
-                command.Line2,
-                command.City,
-                command.State,
-                command.ZipCode) 
-            is not Address address)
-            throw new ArgumentException(nameof(address));
-        
-        var customer = new Customer(
-            new CustomerId(Guid.NewGuid()),
-            command.Name,
-            command.LastName,
-            command.LastName,
-            phoneNumber,
-            address,
-            true
-        );
-
-        await _customerRepository.Add(customer);
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
-
-        return Unit.Value;
+        try
+        {
+            if(PhoneNumber.Create(command.PhoneNumber) is not PhoneNumber phoneNumber)
+                return Error.Validation("Customer.PhoneNumber", "Phone number has no valid format");
+    
+            if(Address.Create(
+                    command.Country,
+                    command.Line1,
+                    command.Line2,
+                    command.City,
+                    command.State,
+                    command.ZipCode) 
+                is not Address address)
+                return Error.Validation("Customer.Address", "Address is not valid");
+            
+            var customer = new Customer(
+                new CustomerId(Guid.NewGuid()),
+                command.Name,
+                command.LastName,
+                command.LastName,
+                phoneNumber,
+                address,
+                true
+            );
+    
+            await _customerRepository.Add(customer);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+    
+            return Unit.Value;
+        }
+        catch (Exception ex)
+        {
+            return Error.Failure("CreateCustomer.Failure", ex.Message);
+        }
     }   
 }
